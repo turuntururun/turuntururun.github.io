21d576c8aab84fcf8a988bc1ad643c5e56726058
Kevin <kevin@turuntururun.com>
2026-07-22 21:28:04 -0600

    removed doc static files

--- docs/_nuxt/BSAqZyc5.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./xAcPgC9U.js";import"./D2uUAb2i.js";export{m as default};
--- docs/_nuxt/CNF9wqg8.js
+++
@@ -1,1 +0,0 @@
-import{e as n,J as e}from"./D2uUAb2i.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
